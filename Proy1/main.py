de3105a8a079603017421e09c328405945c1439f
Hilario <johimesa@gmail.com>
2022-08-04 15:44:16 -0500

    variables, tipos datos, integer, float, conversiones

--- Proy1/main.py
+++ Proy1/main.py
@@ -1,3 +1,22 @@
-#input("Dime tu nombre: ")
-#input("Dime tu apellido: ")
-print("Tu datos son " + input("Nombre: ") + " " + input("Apellidos: "))
+nombre = "Hilario"
+print("Tu nombre es " + nombre)
+
+number = 12
+print(number)
+print(type(number))
+
+number_2 = 5.6
+print(type(number_2))
+
+suma = 89 + 123.09 #conversión implícita
+print("La suma es " + str(suma))
+print(type(suma))
+
+str_uno = "1"
+print(4 + int(str_uno)) #conversion explícita (yo indico por código)
+
+num1 = 7.5
+print(int(num1))
+
+num2 = 4
+print(float(num2))
